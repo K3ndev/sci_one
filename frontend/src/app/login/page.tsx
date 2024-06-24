568f5a7325a401eeb361555f188525306c5b2ec1
K3ndev <racelisjk@gmail.com>
2024-06-24 16:07:25 +0800

    feat: added auth redirect on login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,48 +1,20 @@
-'use client';
 
-import { useState } from 'react';
-import { useFormState } from 'react-dom';
-import { login, signup } from "./actions";
+import { redirect } from 'next/navigation';
+import LoginForm from "./components/form"
+import { createClient } from "../../utils/supabase/server"
 
-export default function LoginPage() {
-  const [loginState, formLogin] = useFormState(login, {
-    error: null,
-  });
-  const [signupState, formSignup] = useFormState(signup, {
-    error: null,
-  });
-
-  const [isLogin, setIsLogin] = useState(true);
+export default async function LoginPage() {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.getUser();
+  
+  if (!error || data?.user) {
+    redirect('/');
+  }
 
   return (
     <>
-      {isLogin ? (
-        <form action={formLogin} className="flex w-60 flex-col">
-          <label htmlFor="email">Email:</label>
-          <input id="email" name="email" type="email" required />
-          <label htmlFor="password">Password:</label>
-          <input id="password" name="password" type="password" required />
-          <button type="submit" className="bg-red-800 text-black">
-            Log in
-          </button>
-          <p className="my-2 text-red-900">{loginState.error}</p>
-        </form>
-      ) : (
-        <form action={formSignup} className="flex w-60 flex-col">
-          <label htmlFor="email">Email:</label>
-          <input id="email" name="email" type="email" required />
-          <label htmlFor="password">Password:</label>
-          <input id="password" name="password" type="password" required />
-          <button type="submit" className="bg-red-800 text-black">
-            Sign up
-          </button>
-          <p className="my-2 text-red-900">{signupState.error}</p>
-        </form>
-      )}
-
-      <div>
-        <button onClick={() => setIsLogin(!isLogin)}>toggle sign in/up</button>
-      </div>
+      <h1>hi</h1>
+      <LoginForm />
     </>
   );
 }
