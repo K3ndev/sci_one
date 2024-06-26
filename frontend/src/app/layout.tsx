650f2d0b65a0905926c6789c8c200ea41f5c26f0
K3ndev <racelisjk@gmail.com>
2024-06-26 13:51:30 +0800

    chore: remove mantine that cause performance when action is triggered in dev

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,6 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css';
-
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-
 export const metadata = {
-  title: 'My Mantine app',
-  description: 'I have followed setup instructions carefully',
+  title: 'SCI_ONE',
+  description: 'SCI_ONE activity',
 };
 
 export default function RootLayout({
@@ -16,11 +10,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <ColorSchemeScript />
-      </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        {children}
       </body>
     </html>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,52 +1,61 @@
-import { Box } from '@mantine/core';
-import { redirect } from 'next/navigation';
-import Nav from "./_components/nav/nav"
-import TodoItem from "./_components/todo-list/todo"
-import { AddTodo } from './_action/add-todo';
-import PaginationButton from "./_components/pagination-button"
-import {getPagination} from "./_utils/get-pagination"
-import { createClient } from '@/utils/supabase/server';
-
+import { redirect } from "next/navigation";
+import Nav from "./_components/nav/nav";
+import TodoItem from "./_components/todo-list/todo";
+import { AddTodo } from "./_action/add-todo";
+import PaginationButton from "./_components/pagination-button";
+import { getPagination } from "./_utils/get-pagination";
+import { createClient } from "@/utils/supabase/server";
 
 type SearchParamsType = {
   [key: string]: string;
 };
 
-export default async function Home({searchParams}: {searchParams: SearchParamsType}) {
-  const supabase = createClient()
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: SearchParamsType;
+}) {
+  const supabase = createClient();
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
-    redirect('/login');
+    redirect("/login");
   }
 
   // pagination
   const { from, to } = getPagination(+searchParams.page || 0, 1);
-  const { data: todos } = await supabase.from('todos').select('*').range(from, to).order('id', { ascending: true });;
+  const { data: todos } = await supabase
+    .from("todos")
+    .select("*")
+    .range(from, to)
+    .order("id", { ascending: true });
 
   return (
-      <>
-        <Nav email={data.user?.email} id={data.user?.id}/>
-        <main>
-            <Box bg="var(--mantine-color-blue-light)">
-              <ul>
-                {todos?.map((item, index: number) => {
-                  return (
-                    <li key={+index + +item.id} className="mb-1 flex gap-2">
-                      <TodoItem id={item.id} isCheck={item.isCheck} todo={item.todo} user_id={item.user_id}/>
-                    </li>
-                  );
-                })}
-              </ul>
-              <form action={AddTodo}>
-                <input type="text" name="newTodo" placeholder="add new todo here" />
-                <button type="submit" className="bg-red-800 text-black">
-                  submit
-                </button>
-              </form>
-            </Box>
-            <PaginationButton />
-        </main>
-      </>
+    <>
+      <Nav email={data.user?.email} id={data.user?.id} />
+      <main>
+        <ul>
+          {todos?.map((item, index: number) => {
+            return (
+              <li key={+index + +item.id} className="mb-1 flex gap-2">
+                <TodoItem
+                  id={item.id}
+                  isCheck={item.isCheck}
+                  todo={item.todo}
+                  user_id={item.user_id}
+                />
+              </li>
+            );
+          })}
+        </ul>
+        <form action={AddTodo}>
+          <input type="text" name="newTodo" placeholder="add new todo here" />
+          <button type="submit" className="bg-red-800 text-black">
+            submit
+          </button>
+        </form>
+        <PaginationButton />
+      </main>
+    </>
   );
 }
 
